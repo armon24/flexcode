eac6d87a8e32674765117995c1eb41da417d224d
Armon <armon24@vt.edu>
2018-11-27 08:25:21 -0500

    this commit

---
+++ furtherRegistration.php
@@ -0,0 +1,225 @@
+    <br>
+    <table>
+
+    <tr>
+        <td>
+            <label  id="type">Where are you from? </label>
+        </td>
+        <td>
+            <input type="text" name="state" placeholder="Select a state" list="stateList" />
+                
+                <datalist id="stateList">
+                    <option value="Alabama">
+                    <option value="Alaska">
+                    <option value="Arizona">
+                    <option value="Arkansas">
+                    <option value="California">
+                    <option value="Colorado">
+                    <option value="Connecticut">
+                    <option value="Delaware">
+                    <option value="Florida">
+                    <option value="Georgia">
+                    <option value="Hawaii">
+                    <option value="Idaho">
+                    <option value="Illinois">
+                    <option value="Indiana">
+                    <option value="Iowa">
+                    <option value="Kansas">
+                    <option value="Kentucky">
+                    <option value="Louisiana">
+                    <option value="Maine">
+                    <option value="Maryland">
+                    <option value="Massachusetts">
+                    <option value="Michigan">
+                    <option value="Minnesota">
+                    <option value="Mississippi">
+                    <option value="Missouri">
+                    <option value="Montana">
+                    <option value="Nebraska">
+                    <option value="Nevada">
+                    <option value="New Hampshire">
+                    <option value="New Jersey">
+                    <option value="New Mexico">
+                    <option value="North Carolina">
+                    <option value="North Dakota">
+                    <option value="Ohio">
+                    <option value="Oklahoma">
+                    <option value="Oregon">
+                    <option value="Pennsylvania">
+                    <option value="Rhode Island">
+                    <option value="South Carolina">
+                    <option value="South Dakota">
+                    <option value="Tennessee">
+                    <option value="Texas">
+                    <option value="Utah">
+                    <option value="Vermont">
+                    <option value="Virginia">
+                    <option value="West Virginia">
+                    <option value="Wisconsin">
+                    <option value="Wyoming">
+                </datalist>
+        </td>
+    </tr>
+
+    <tr>
+        <td>
+            <label  id="type">What's your major? </label>
+        </td>
+        <td>
+            <input type="text" name="major" placeholder="Select a major" list="majorList" />
+                
+                <datalist id="majorList">
+                    <option value="Accounting and Information Systems">
+                    <option value="Business Information Technology">
+                    <option value="Economics">
+                    <option value="Finance">
+                    <option value="Management">
+                    <option value="Marketing">
+                    <option value="Real Estate">
+                    <option value="Not listed">
+                </datalist>
+        </td>
+    </tr>
+
+    <tr>
+        <td>
+            <label  id="type">What's your minor? (If none, select 'Not listed')</label>
+        </td>
+        <td>
+            <input type="text" name="minor" placeholder="Select a minor" list="minorList" />
+                
+                <datalist id="minorList">
+                    <option value="Digital Marketing Strategy">
+                    <option value="Entrepreneurship-New Venture Growth">
+                    <option value="International Business">
+                    <option value="Leadership">
+                    <option value="Professional Sales">
+                    <option value="Other (College of Engineering, College of Science, etc.)">
+                    <option value="Not listed">
+                </datalist>
+        </td>
+    </tr>
+    
+    <tr>
+        <td>
+            <label  id="type">What's your favorite on-campus place to eat? </label>
+        </td>
+        <td>
+            <input type="text" name="food" placeholder="Select a dining hall" list="foodList" />
+                
+                <datalist id="foodList">
+                    <option value="Turner Place">
+                    <option value="West End Market">
+                    <option value="D2">
+                    <option value="DX">
+                    <option value="Deets Place">
+                    <option value="Owens Food Court">
+                    <option value="Burger 37">
+                    <option value="ABP">
+                    <option value="Not listed">
+                </datalist>
+        </td>
+    </tr>
+
+    <tr>
+        <td>
+            <label  id="type">What are your hobbies? </label>
+        </td>
+        <td>
+            <input type="text" name="hobby" placeholder="Select a hobby" list="hobbyList" />
+                
+                <datalist id="hobbyList">
+                    <option value="Art">            <label>(Ex: Photography, painting, etc.)</label>
+                    <option value="Music">          <label>(Ex: In a band, with friends or independently, etc.)</label>
+                    <option value="Sports">         <label>(Ex: On a team, intramural or with friends, etc.)</label>
+                    <option value="Videogames">     <label>(Ex: On a team, casually or with friends etc.)</label>
+                    <option value="Other">          <label>(Ex: Something unique to you!)</label>
+                </datalist>
+            
+        </td>
+    </tr>
+
+    <tr>
+        <td>
+            <label  id="type">What's your favorite area on campus? </label>
+        </td>
+        <td>
+            <input type="text" name="place" placeholder="Select a place" list="placeList" />
+                
+                <datalist id="placeList">
+                    <option value="Drillfield">
+                    <option value="Pylons/War Memorial Chapel">
+                    <option value="Torgerson Bridge">
+                    <option value="Newman Library">
+                    <option value="War Memorial Gym">
+                    <option value="McComas Gym">
+                    <option value="Squires">
+                    <option value="Burruss Hall">
+                    <option value="Cassell Coliseum">
+                    <option value="Lane Stadium">
+                    <option value="Hahn Horticulture Garden">
+                    <option value="Not listed">
+                </datalist>
+            
+        </td>
+    </tr>
+
+    <tr>
+        <td>
+            <label  id="type">Where did you live/where are you living on-campus? </label>
+        </td>
+        <td>
+            <input type="text" name="dorm" placeholder="Select a dorm" list="dormList" />
+                
+                <datalist id="dormList">
+                    <option value="East Ambler Johnston">
+                    <option value="West Ambler Johnston">
+                    <option value="Barringer Hall">
+                    <option value="Main Campbell Hall">
+                    <option value="East Campbell Hall">
+                    <option value="Cochrane Hall">
+                    <option value="East Eggleston Hall">
+                    <option value="Main Eggleston Hall">
+                    <option value="West Eggleston Hall">
+                    <option value="Hillcrest Hall">
+                    <option value="Harper Hall">
+                    <option value="Johnson Hall">
+                    <option value="Lee Hall">
+                    <option value="Miles Hall">
+                    <option value="New Hall West">
+                    <option value="Newman Hall">
+                    <option value="New Residence Hall East">
+                    <option value="Pritchard">
+                    <option value="O'Shaughnessy Hall">
+                    <option value="Payne Hall">
+                    <option value="Slusher Hall">
+                    <option value="Peddrew-Yates Hall">
+                    <option value="Pearson Hall">
+                    <option value="New Cadet Hall">
+                    <option value="Vawter Hall">
+                </datalist>
+        </td>
+    </tr>
+
+    <tr>
+        <td>
+            <label  id="type"> What kind of primary advice are you seeking? </label>
+        </td>
+        <td>
+            <input type="text" name="advice" placeholder="Select a concept" list="adviceList" />
+                
+                <datalist id="adviceList">
+                    <option value="Academic"> <label>(Ex: course selection and Professor suggestions)</label>
+                    <option value="Social">   <label>(Ex: Clubs and Greek life)</label>
+                    <option value="Career">   <label>(Ex: Internships, Co-Ops and Research)</label>
+                    <option value="Other">    <label>(Ex: 'Just looking for life advice!')>
+                </datalist>
+        </td>
+    </tr>
+
+</table>
+
+<br>
+
+<input type="submit" name="registerButton" value="Register!" />
+    
---
+++ homepage.php
@@ -0,0 +1,34 @@
+
+
+<!doctype html>
+
+<html>
+
+    <head>
+        
+        <!-- Background Stuff -->
+        <title> Homepage | MentorMatch </title>
+        <meta charset="utf-8"/>
+
+        <!-- Link to our overall CSS sheet -->
+        <!-- Link to our overall JavaScript file: <script src="main.js"></script>  -->
+
+        <!-- Style for this sheet only -->
+        <style>
+        
+        </style>
+
+        <!-- Functions written in JavaScript right here -->
+        <script type="text/javascript">
+ 
+        </script>
+
+    </head>
+
+    <body>
+
+            <p> YE YE </p>
+      
+    </body>
+
+</html>
