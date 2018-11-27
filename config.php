6e34dbc5f28fe8d4decbf8aabf8faa35fce11642
purnima <purnima@vt.edu>
2018-11-27 10:02:07 -0500

    Working on beautification

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
   define('DB_HOST', "localhost");  
   define('DB_USER', "root");
-  define('DB_PASS', "");
-  define('DB_NAME', "web-based dss");
+  define('DB_PASS', "CSD@mysql-1872");
+  define('DB_NAME', "BIT4444");
 
   //replace lunchDB with the name of our database in particular
  ?>
